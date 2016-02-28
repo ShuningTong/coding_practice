bff96850df1b1ab8c73024fbb25dfad013d109cf
Shuning Tong <sht72@pitt.edu>
2016-02-27 22:46:04 -0500

    finish 0208

--- 0208/AnagramPartition.java
+++ 0208/AnagramPartition.java
@@ -1,3 +1,4 @@
+
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Map;
@@ -5,13 +6,38 @@ import java.util.HashMap;
 
 public class AnagramPartition
 {
+	public static void main(String[] args){
+		String[] dictionary = {"reset", "cider", "acres", "cried", "steer", "dicer", "cares", "trees", "ester", "terse"};
+		partition(dictionary);
+	}
     public static void partition(String[] dictionary) {
         // use a HashMap to store all the words that are anagrams to each other
         // in a array, key is a string of representation of this group of 
         // anagrams
 
-
-
-        // Then print out anagrams group by group
-    }
+    	HashMap<String, ArrayList<Integer>> map = new HashMap<String, ArrayList<Integer>>();
+    	for(int i = 0; i < dictionary.length; i++){
+    		char[] word = dictionary[i].toCharArray();
+    		Arrays.sort(word);
+    		String sortedWord = String.valueOf(word);
+    		if(map.containsKey(sortedWord)){
+    			map.get(sortedWord).add(i);
+    		}else{
+    			ArrayList<Integer> new_map = new ArrayList<Integer>();
+    			new_map.add(i);
+    			map.put(sortedWord, new_map);
+    		}
+    	}
+    	
+    	for (Map.Entry<String, ArrayList<Integer>> entry: map.entrySet()){
+    		String key = entry.getKey();
+    		ArrayList<Integer> value = entry.getValue();
+    		for(Integer i: value){
+    			System.out.print(dictionary[i] + " ");
+    		}
+    		System.out.println("");
+    	}
+    	
+    	
+}
 }
--- 0208/SearchFirstTarget.java
+++ 0208/SearchFirstTarget.java
@@ -1,6 +1,70 @@
+
 public class SearchFirstTarget
 {
-    public static int search(int k, int[] a) {
-    
+	public static void main(String[] args){
+		int k = 45;
+		int[] a = {1, 5, 7, 7, 7, 8, 9, 10, 23, 45, 45, 45, 67};
+		search(k, a);
+	}
+    public static void search(int k, int[] a) {
+    	/*int[] a = null;
+    	int k = 0;
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("Enter sorted array: ");
+    	if(sc.hasNextLine()){
+    		String string = sc.nextLine();
+    		String[] ss = string.split(" ");
+    		int length = ss.length;
+    		a = new int[length];
+    		for (int i = 0; i < length; i++){
+    			int e = Integer.parseInt(ss[i]);
+    			a[i] = e;
+    		}
+    	}
+    	System.out.println("Enter the key you want to search: ");
+    	if(sc.hasNextInt()){
+    		k = sc.nextInt();
+    	}*/
+    	
+    	int start = 0;
+    	int end = a.length - 1;
+    	int mid = (start + end)/2;
+    	
+    	outerloop:
+    	while(true){
+    		// if the key is larger than mid value
+    		if(k > a[mid]){
+        		start = mid + 1;
+        		// once start value is larger than end value, we know key does not exist in array
+        		if (start > end){
+        			System.out.println("Not found!");
+        			break outerloop;
+        		}
+        	// if the key is smaller than mid value
+        	}else if(k < a[mid]){
+        		end = mid - 1;
+        		// once start value is larger than end value, we know key does not exist in array
+        		if (end < start){
+        			System.out.println("Not found!");
+        			break outerloop;
+        		}
+        	// if the key is equal to mid value
+        	}else{
+        		// we iterate from mid value to start value to look for the first occurance
+        		for (int i = mid - 1; i >= start; i++){
+        			if(a[i] != k){
+        				mid = i + 1;
+                		System.out.println("Found at the " + mid + "th position");
+        				break outerloop;
+        			}
+        		}
+        		// if all values from mid to start are all equal to key value, we know the first occurance is the start
+        		mid = start;
+        		System.out.println("Found at the " + mid + "th position");
+        		break outerloop;
+        	}
+    		mid = (start + end)/2;
+    	}
+    	
     }
 }
